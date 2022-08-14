04aa19fc0d8af255b1066640fa4beb19900d5b9e
Jon-Mikel <garrisonhgrave@yahoo.com>
2022-08-14 12:45:33 -0500

    added data to server

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 app.use(express.static('public'))
 app.set("view engine", "ejs")
 
-const data = require('./')
+const data = require('./data/fakeData.json')
+console.log(typeof(data))
 
 // ROUTING
 app.get('/', (req, res)=>{
@@ -14,6 +15,8 @@ app.get('/', (req, res)=>{
 app.get('/landing', (req, res)=>{
   res.render("landing")
 })
+
+
 
 const port = process.env.PORT || 3000
 app.listen(port, ()=> console.log(`App listening on port ${port}`))
